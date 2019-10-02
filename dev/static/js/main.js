073f3a9795af1689077edd72982258ca2f995312
muslimevlo <muslimevlo@yandex.ru>
2019-10-02 17:25:47 +0300

    делаем адаптив слайда

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -85,7 +85,33 @@ $(document).ready(function () {
     let productLineSlider = function () {
         $('.js-products-line-slider').slick({
             slidesToShow: 4,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            responsive: [
+              {
+                breakpoint: 1139,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+          });
         })
     };
     mainSubnavHover();
